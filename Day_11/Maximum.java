b93b6d8bd465d701d704e64184dcba2f9b5f97c1
Tarun Srivastav <tarunsrivastav1611@gmail.com>
2026-06-11 21:59:15 +0530

    Done!!

---
+++ Day_11/Maximum.java
@@ -0,0 +1,23 @@
+package Day_11;
+import java.util.Scanner;
+public class Maximum 
+{   
+    static int findMax(int a, int b)
+     {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+    public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int max = findMax(num1, num2);
+        System.out.println("Maximum number = " + max);
+        sc.close();
+    }
+}
