c5281ed5b3c55503662a39f396062d2d74da8484
Tarun Srivastav <tarunsrivastav1611@gmail.com>
2026-06-12 21:19:30 +0530

    ..

---
+++ Day_12/Armstrong.java
@@ -0,0 +1,34 @@
+package Day_12;
+import java.util.Scanner;
+
+public class Armstrong {
+
+    public static boolean isArmstrong(int num) {
+        int originalNum = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+
+        return sum == originalNum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isArmstrong(num)) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not an Armstrong Number");
+        }
+
+        sc.close();
+    }
+}
